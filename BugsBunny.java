7a7507cbd686c52f6b3a3031464d3914a3890881
Ganavi M S <ganavims2006@gmail.com>
2025-12-20 23:56:01 +0530

    Bugs Bunny Game

---
+++ BugsBunny.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BugsBunny{   
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Tell me a Number:");
+        int x=sc.nextInt();
+
+        int bugs=x % 2; //even
+        int bunny= x % 3;   //odd
+        int ans=0;
+        
+        if(bugs==ans && bunny==ans){
+            System.out.println("Hyyy!!...its a tie");
+        }
+        else if(ans==bugs){
+            System.out.println("Hyyy!!...Bugs Winner!!");
+        }else if(ans==bunny){
+            System.out.println("Hyyy!!...Bunny Winner!!");
+        }else{
+            System.out.println("guess your luck number again.");
+        }
+         sc.close();
+}
+}
+
+// just to practice and making comfortable with java.
